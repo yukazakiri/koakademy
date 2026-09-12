ecab544bcf74c6c70d8b1ac57b9e0508b4c5b60f
Yuka <128908897+yukazakiri@users.noreply.github.com>
2026-09-13 02:39:40 +0800

    feat: support Philippine profile location controls (#269)

--- resources/js/components/ui/combobox.tsx
+++ resources/js/components/ui/combobox.tsx
@@ -111,7 +111,7 @@ export function Combobox({
             disabled={disabled}
           >
             <span className="truncate">
-              {selectedOption ? selectedOption.label : placeholder}
+              {selectedOption ? selectedOption.label : value || placeholder}
             </span>
             <IconChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>

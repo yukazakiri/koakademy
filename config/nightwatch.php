7552e58f1ea47aee1769d2f6e529503d15afe7a9
Yuka <128908897+yukazakiri@users.noreply.github.com>
2026-09-14 22:41:07 +0000

    feat(observability): configure Laravel Nightwatch

---
+++ config/nightwatch.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'enabled' => env('NIGHTWATCH_ENABLED', true),
+    'token' => env('NIGHTWATCH_TOKEN'),
+    'deployment' => env('NIGHTWATCH_DEPLOY', env('LARAVEL_CLOUD_DEPLOY_UUID', env('FORGE_DEPLOY_COMMIT', env('VAPOR_COMMIT_HASH')))),
+    'server' => env('NIGHTWATCH_SERVER', (string) gethostname()),
+    'capture_exception_source_code' => env('NIGHTWATCH_CAPTURE_EXCEPTION_SOURCE_CODE', true),
+    'capture_request_payload' => env('NIGHTWATCH_CAPTURE_REQUEST_PAYLOAD', false),
+    'redact_payload_fields' => explode(',', env('NIGHTWATCH_REDACT_PAYLOAD_FIELDS', '_token,password,password_confirmation')),
+    'redact_headers' => explode(',', env('NIGHTWATCH_REDACT_HEADERS', 'Authorization,Cookie,Proxy-Authorization,X-XSRF-TOKEN')),
+
+    'sampling' => [
+        'requests' => env('NIGHTWATCH_REQUEST_SAMPLE_RATE', 1.0),
+        'commands' => env('NIGHTWATCH_COMMAND_SAMPLE_RATE', 1.0),
+        'exceptions' => env('NIGHTWATCH_EXCEPTION_SAMPLE_RATE', 1.0),
+        'scheduled_tasks' => env('NIGHTWATCH_SCHEDULED_TASK_SAMPLE_RATE', 1.0),
+    ],
+
+    'filtering' => [
+        'ignore_cache_events' => env('NIGHTWATCH_IGNORE_CACHE_EVENTS', false),
+        'ignore_mail' => env('NIGHTWATCH_IGNORE_MAIL', false),
+        'ignore_notifications' => env('NIGHTWATCH_IGNORE_NOTIFICATIONS', false),
+        'ignore_outgoing_requests' => env('NIGHTWATCH_IGNORE_OUTGOING_REQUESTS', false),
+        'ignore_queries' => env('NIGHTWATCH_IGNORE_QUERIES', false),
+        'log_level' => env('NIGHTWATCH_LOG_LEVEL', env('LOG_LEVEL', 'debug')),
+    ],
+
+    'ingest' => [
+        'uri' => env('NIGHTWATCH_INGEST_URI', '127.0.0.1:2407'),
+        'timeout' => env('NIGHTWATCH_INGEST_TIMEOUT', 0.5),
+        'connection_timeout' => env('NIGHTWATCH_INGEST_CONNECTION_TIMEOUT', 0.5),
+    ],
+];
